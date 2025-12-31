383d9d12c386e84f693b80237229b3e12a373428
Sudhanshu Thapa <sudhanshuthapa8@gmail.com>
2026-01-01 01:30:39 +0530

    activities page cleared

--- uk/src/app/activities/page.tsx
+++ uk/src/app/activities/page.tsx
@@ -1,12 +1,10 @@
-import { getActivitiesList } from '@/lib/db/getActivitiesList';
-import ActivitiesClient from './Activities.client';
+import { getActivitiesList } from "@/lib/db/getActivitiesList";
+import ActivitiesClient from "./Activities.client";
 
 export default async function ActivitiesPage() {
   const initialActivities = await getActivitiesList({
     limit: 12,
   });
 
-  return (
-    <ActivitiesClient initialActivities={initialActivities} />
-  );
+  return <ActivitiesClient initialActivities={[]} />;
 }
