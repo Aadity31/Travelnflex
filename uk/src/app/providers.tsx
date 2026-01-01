a175d00b4c6625288da6511eefcfc3277890e863
Aadity31 <adityadimri04@gmail.com>
2026-01-01 23:57:55 +0530

    update thapa

--- uk/src/app/providers.tsx
+++ uk/src/app/providers.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import LoadingOverlay from "./components/LoadingOverlay"; // ✅ Import yaha
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <LoadingOverlay />
+    
       {children}
     </SessionProvider>
   );
