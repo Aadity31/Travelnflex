44eff7a7f353b41e5c2a21b1d431a567a2395b0b
Aadity31 <adityadimri04@gmail.com>
2026-02-10 21:17:08 +0530

    FIX

--- uk/src/components/confirm_booking/ItinerarySection.tsx
+++ uk/src/components/confirm_booking/ItinerarySection.tsx
@@ -319,11 +319,7 @@ function DayItem({
           </div>
         )}
 
-<<<<<<< HEAD
         {/* Collapsed hint */}
-=======
-        
->>>>>>> 1670035df9dedfb60c1ec80f499faab46a8b9efd
       </div>
     </div>
   );
